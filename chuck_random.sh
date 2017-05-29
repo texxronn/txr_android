672c3a5411f6cba4d30536b12ad7dd13d4288fc2
texxronn <texxronn@users.noreply.github.com>
2017-05-29 20:40:04 +1000

    Chuck Joke Random Script

---
+++ chuck_random.sh
@@ -0,0 +1,2 @@
+CHUCK_JOKE=$(curl -s https://api.chucknorris.io/jokes/random |jq -r '.value')
+am broadcast  --user 0 -a org.kustom.action.SEND_VAR -e org.kustom.action.EXT_NAME ron -e org.kustom.action.VAR_NAME qod -e org.kustom.action.VAR_VALUE "$CHUCK_JOKE"
