ee9e0d186a673085899d512ee6a50f1006ba34b4
Ron Mac <texx.ronn@gmail.com>
2017-06-02 20:17:13 +1000

    train script fixed

--- googTrain.sh
+++ googTrain.sh
@@ -2,7 +2,15 @@ STJAMES=ChIJcexqOBWuEmsRK1a72WYib80
 INGLEBURN=ChIJse9sV3zrEmsRzOD__WvL23Q
 KEY=AIzaSyAohDqg5TiFUKm7ctK_L5c1N2ZSE7pK-Gg
 
-PLACE2="place_id:$STJAMES"
-PLACE1="place_id:$INGLEBURN"
+if [ "I" = "$1" ]; then
+	PLACE1="place_id:$STJAMES"
+	PLACE2="place_id:$INGLEBURN"
+else
+	PLACE2="place_id:$STJAMES"
+	PLACE1="place_id:$INGLEBURN"
+fi
 
-curl -s https://maps.googleapis.com/maps/api/directions/json\?origin\=$PLACE1\&destination\=$PLACE2\&mode\=transit\&key\=$KEY | jq '.routes[] |.legs[]|.steps[]|.html_instructions + " from " + .transit_details.departure_stop.name + " at " + .transit_details.departure_time.text '
+curl -s https://maps.googleapis.com/maps/api/directions/json\?origin\=$PLACE1\&destination\=$PLACE2\&mode\=transit\&alternatives=true\&key\=$KEY | \
+#jq '.routes[] |.legs[0].departure_time.text + .legs[0].start_address|split(",")'
+jq '.routes[] |.legs[0].departure_time.value'\
+|xargs -i date -d @{} "+%H:%M"
--- qod.sh
+++ qod.sh
@@ -1,2 +1,3 @@
 QT=$(curl -s http://quotes.rest/qod.json |jq -r '.contents.quotes[0].quote')
+echo $QT
 am broadcast --user 0 -a org.kustom.action.SEND_VAR -e org.kustom.action.EXT_NAME ron -e org.kustom.action.VAR_NAME DASH_CONST -e org.kustom.action.VAR_VALUE "$QT"
