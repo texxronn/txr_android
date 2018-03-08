3fa62852b402a366eb061d3322de42152a8d9a2b
Ron Mac <texx.ronn@gmail.com>
2018-03-09 09:58:15 +1100

    Re-org

--- bible_vod.sh
+++
@@ -1,1 +0,0 @@
-VOTD=$(curl -s http://labs.bible.org/api/?passage=votd&type=json |jq -r '.contents.quotes[0].quote')
--- chuck.sh
+++
@@ -1,2 +0,0 @@
-QT=$(curl -s https://api.chucknorris.io/jokes/random |jq -r '.value')
-am broadcast --user 0 -a com.ron.chucktext -e chucktext "$QT"
